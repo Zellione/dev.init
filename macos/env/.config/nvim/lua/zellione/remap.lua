dc9389263f9d471759e029761f9bac827c9d4eb4
Zellione <nagels.sebastian@gmail.com>
2026-06-01 23:56:59 +0200

    add <C-h/j/k/l> window navigation

--- macos/env/.config/nvim/lua/zellione/remap.lua
+++ macos/env/.config/nvim/lua/zellione/remap.lua
@@ -7,6 +7,12 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
 -- vim.keymap.set("n", "n", "nzzzv")
 -- vim.keymap.set("n", "N", "Nzzzv")
 
