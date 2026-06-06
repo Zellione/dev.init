0fbb97706615c336de9b005103662f91c3157ecd
Zellione <nagels.sebastian@gmail.com>
2026-06-06 21:49:45 +0200

    update treesitter config

--- common/env/.config/nvim/lua/zellione/lazy/treesitter.lua
+++ common/env/.config/nvim/lua/zellione/lazy/treesitter.lua
@@ -1,13 +1,18 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+    branch = "main",
 	lazy = false,
-	build = ":TSUpdate",
+    build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter").setup {
 			install_dir = vim.fn.stdpath("data") .. "/site",
+            auto_install = true,
+            highlight = {
+                enable = true,
+            }
 		}
 
-		require("nvim-treesitter").install {
+		local ensure_installed = {
 			"bash",
 			"c",
 			"cpp",
@@ -24,8 +29,24 @@ return {
 			"go",
 		}
 
-		-- Register markdown_inline as an embedded language of markdown so
-		-- treesitter-context (and highlighting) follow it inside markdown files.
+		local installed = require("nvim-treesitter.config").get_installed()
+		local to_install = vim.iter(ensure_installed)
+			:filter(function(parser)
+				return not vim.tbl_contains(installed, parser)
+			end)
+			:totable()
+
+		if #to_install > 0 then
+			require("nvim-treesitter").install(to_install)
+		end
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
+
 		pcall(vim.treesitter.language.register, "markdown", "markdown_inline")
 	end,
 }
