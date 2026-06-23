9101a37a617042707f9f67313028f301ca05dd20
sna06 <sebastian_nagels@deichmann.com>
2026-06-23 16:29:07 +0200

    chore(nvim): replaced nvim-notify with snacks

--- common/env/.config/nvim/lua/zellione/lazy/snacks.lua
+++ common/env/.config/nvim/lua/zellione/lazy/snacks.lua
@@ -3,6 +3,11 @@ return {
 	priority = 1000,
 	lazy = false,
 	opts = {
+		-- Enable Notifier
+		notifier = {
+			enabled = true,
+			timeout = 3000,
+		},
 		-- Enable the input UI replacement
 		input = {
 			enabled = true,
@@ -13,5 +18,43 @@ return {
 			enabled = true,
 		},
 		-- ... your other snacks configs (dashboard, notifier, etc.)
+	},
+	init = function()
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "VeryLazy",
+			callback = function()
+				-- 3. Override Neovim's default print/notify with Snacks
+                ---@diagnostic disable: duplicate-set-field
+				_G.dd = function(...)
+					Snacks.debug.inspect(...)
+				end
+				_G.bt = function()
+					Snacks.debug.backtrace()
+				end
+                ---@diagnostic enable: duplicate-set-field
+				vim.print = _G.dd -- Override print to use snacks preview
+
+				-- Create a smooth transition for any standard vim.notify calls
+				Snacks.toggle.profiler():map("<leader>dpp")
+			end,
+		})
+	end,
+	keys = {
+		-- Useful keymap to view your notification history
+		{
+			"<leader>nh",
+			function()
+				Snacks.notifier.show_history()
+			end,
+			desc = "Notification History",
+		},
+		-- Clear active notifications on screen
+		{
+			"<leader>nd",
+			function()
+				Snacks.notifier.hide()
+			end,
+			desc = "Dismiss Notifications",
+		},
 	},
 }
