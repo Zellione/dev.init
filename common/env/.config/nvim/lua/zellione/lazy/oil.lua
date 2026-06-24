4641713bf97023a341600c0506ed9a899f5fcba5
sna06 <sebastian_nagels@deichmann.com>
2026-06-24 13:53:30 +0200

    feat(nvim): add oil companion plugin to highlight git status

--- common/env/.config/nvim/lua/zellione/lazy/oil.lua
+++ common/env/.config/nvim/lua/zellione/lazy/oil.lua
@@ -1,33 +1,55 @@
 return {
-	"stevearc/oil.nvim",
-	---@module 'oil'
-	---@type oil.SetupOpts
-	opts = {
-		use_default_keymaps = false,
-		view_options = {
-			-- Show files and directories that start with "."
-			show_hidden = true,
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {
+			use_default_keymaps = false,
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
+			},
+			win_options = {
+				signcolumn = "yes:2",
+			},
+			keymaps = {
+				["g?"] = { "actions.show_help", mode = "n" },
+				["<CR>"] = "actions.select",
+				["<C-p>"] = "actions.preview",
+				["-"] = { "actions.parent", mode = "n" },
+				["_"] = { "actions.open_cwd", mode = "n" },
+				["`"] = { "actions.cd", mode = "n" },
+				["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+				["gs"] = { "actions.change_sort", mode = "n" },
+				["gx"] = "actions.open_external",
+				["g."] = { "actions.toggle_hidden", mode = "n" },
+				["g\\"] = { "actions.toggle_trash", mode = "n" },
+			},
 		},
-		keymaps = {
-			["g?"] = { "actions.show_help", mode = "n" },
-			["<CR>"] = "actions.select",
-			["<C-p>"] = "actions.preview",
-			["-"] = { "actions.parent", mode = "n" },
-			["_"] = { "actions.open_cwd", mode = "n" },
-			["`"] = { "actions.cd", mode = "n" },
-			["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-			["gs"] = { "actions.change_sort", mode = "n" },
-			["gx"] = "actions.open_external",
-			["g."] = { "actions.toggle_hidden", mode = "n" },
-			["g\\"] = { "actions.toggle_trash", mode = "n" },
+		keys = {
+			{ "<leader>pv", "<CMD>Oil<CR>", desc = "Open File explorer" },
 		},
+		-- Optional dependencies
+		dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+		lazy = false,
 	},
-	keys = {
-		{ "<leader>pv", "<CMD>Oil<CR>", desc = "Open File explorer" },
+	{
+		"refractalize/oil-git-status.nvim",
+		dependencies = { "stevearc/oil.nvim" },
+		config = function()
+			require("oil-git-status").setup({
+				show_ignored = true, -- Crucial: Tells the plugin to mark gitignored items
+				symbols = {
+					index = {
+						["!"] = "!", -- Symbol that appears for ignored files
+					},
+					working_tree = {
+						["!"] = "!",
+					},
+				},
+			})
+		end,
 	},
-	-- Optional dependencies
-	dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-	lazy = false,
 }
