b6b4621ea577e51858a78520fa098d1d66bccda4
sna06 <sebastian_nagels@deichmann.com>
2026-06-26 12:17:54 +0200

    feat(nvim): use snacks as diagnostics picker and configure clang

--- common/env/.config/nvim/lua/zellione/lazy/snacks.lua
+++ common/env/.config/nvim/lua/zellione/lazy/snacks.lua
@@ -24,14 +24,14 @@ return {
 			pattern = "VeryLazy",
 			callback = function()
 				-- 3. Override Neovim's default print/notify with Snacks
-                ---@diagnostic disable: duplicate-set-field
+				---@diagnostic disable: duplicate-set-field
 				_G.dd = function(...)
 					Snacks.debug.inspect(...)
 				end
 				_G.bt = function()
 					Snacks.debug.backtrace()
 				end
-                ---@diagnostic enable: duplicate-set-field
+				---@diagnostic enable: duplicate-set-field
 				vim.print = _G.dd -- Override print to use snacks preview
 
 				-- Create a smooth transition for any standard vim.notify calls
@@ -55,6 +55,20 @@ return {
 				Snacks.notifier.hide()
 			end,
 			desc = "Dismiss Notifications",
+		},
+		{
+			"<leader>xd",
+			function()
+				Snacks.picker.diagnostics()
+			end,
+			desc = "Project Diagnostics",
+		},
+		{
+			"<leader>xD",
+			function()
+				Snacks.picker.diagnostics({ filter = { buf = 0 } })
+			end,
+			desc = "Buffer Diagnostics",
 		},
 	},
 }
--- common/env/.config/nvim/lua/zellione/lsp/clangd.lua
+++ common/env/.config/nvim/lua/zellione/lsp/clangd.lua
@@ -1,1 +1,12 @@
-return {}
+return {
+	cmd = {
+		"clangd",
+		"--background-index",
+		"--clang-tidy",
+		"-header-insertion=never",
+	},
+	root_dir = function(fname)
+		local util = require("lspconfig.util")
+		return util.root_pattern("compile_commands.json", "compile_flags.txt", ".git")(fname)
+	end,
+}
