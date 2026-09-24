ccfc03ead35f2a651b81858e057446dfeb6d745d
sna06 <sebastian_nagels@deichmann.com>
2026-09-24 10:15:38 +0200

    chore: nvim + opencode

--- common/env/.config/nvim/lua/zellione/lazy/conform.lua
+++ common/env/.config/nvim/lua/zellione/lazy/conform.lua
@@ -1,3 +1,18 @@
+-- stop_after_first applies to the WHOLE formatters_by_ft list, not just a
+-- sub-group, so it can't be combined with a leading always-run formatter
+-- like trim_whitespace (it would stop right there and skip prettier
+-- entirely). Use conform's documented `first()` recipe instead.
+local function first(bufnr, ...)
+	local conform = require("conform")
+	for i = 1, select("#", ...) do
+		local formatter = select(i, ...)
+		if conform.get_formatter_info(formatter, bufnr).available then
+			return formatter
+		end
+	end
+	return select(1, ...)
+end
+
 return {
 	"stevearc/conform.nvim",
 	opts = {
@@ -9,10 +24,18 @@ return {
 		formatters_by_ft = {
 			lua = { "trim_whitespace", "stylua" },
 			python = { "trim_whitespace", "isort", "black" },
-			javascript = { "trim_whitespace", "prettierd", "prettier", stop_after_first = true },
-			typescript = { "trim_whitespace", "prettierd", "prettier", stop_after_first = true },
-			html = { "trim_whitespace", "prettierd", "prettier", stop_after_first = true },
-			scss = { "trim_whitespace", "prettierd", "prettier", stop_after_first = true },
+			javascript = function(bufnr)
+				return { "trim_whitespace", first(bufnr, "prettierd", "prettier") }
+			end,
+			typescript = function(bufnr)
+				return { "trim_whitespace", first(bufnr, "prettierd", "prettier") }
+			end,
+			html = function(bufnr)
+				return { "trim_whitespace", first(bufnr, "prettierd", "prettier") }
+			end,
+			scss = function(bufnr)
+				return { "trim_whitespace", first(bufnr, "prettierd", "prettier") }
+			end,
 			json = { "trim_whitespace", "jq" },
 			go = { "trim_whitespace", "goimports", "gofmt" },
 		},
@@ -24,6 +47,34 @@ return {
 				},
 				command = "perl",
 				args = { "-pi", "-e", [[s/[ \t]+$//]] },
+			},
+			prettier = {
+				prepend_args = function(_, ctx)
+					local config_names = {
+						".prettierrc",
+						".prettierrc.json",
+						".prettierrc.yml",
+						".prettierrc.yaml",
+						".prettierrc.js",
+						".prettierrc.cjs",
+						".prettierrc.mjs",
+						"prettier.config.js",
+						"prettier.config.cjs",
+						"prettier.config.mjs",
+					}
+					local found = vim.fs.find(config_names, {
+						upward = true,
+						path = vim.fs.dirname(ctx.filename),
+					})
+					if #found > 0 then
+						return {}
+					end
+					return {
+						"--no-semi",
+						"--single-quote",
+						"--no-bracket-spacing",
+					}
+				end,
 			},
 		},
 	},
